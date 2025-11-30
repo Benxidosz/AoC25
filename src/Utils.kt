443569cd871f2abf548454afa01e6c71a2b833da
Benxidosz <szabo.bence.sandor@gmail.com>
2025-11-30 03:37:20 +0100

    Position data class, helper functions

--- src/Utils.kt
+++ src/Utils.kt
@@ -19,3 +19,66 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+data class IPos(var x: Int, var y: Int) {
+    operator fun plus(other: IPos) = IPos(x + other.x, y + other.y)
+    operator fun minus(other: IPos) = IPos(x - other.x, y - other.y)
+    operator fun times(other: Int) = IPos(x * other, y * other)
+    operator fun div(other: Int) = IPos(x / other, y / other)
+    operator fun plusAssign(other: IPos) {
+        x += other.x
+        y += other.y
+    }
+    operator fun minusAssign(other: IPos) {
+        x -= other.x
+        y -= other.y
+    }
+    operator fun timesAssign(other: Int) {
+        x *= other
+        y *= other
+    }
+    operator fun divAssign(other: Int) {
+        x /= other
+        y /= other
+    }
+    operator fun unaryMinus() = IPos(-x, -y)
+    fun rotate90CW() = IPos(y, x * -1)
+    fun rotate90CCW() = IPos(y * -1, x)
+    fun length() = x + y
+}
+
+fun <T> List<T>.containsAny(other: List<T>): Boolean {
+    other.forEach {
+        if (contains(it)) {
+            return true
+        }
+    }
+    return false
+}
+
+fun <T> List<List<T>>.isValidIndex(pos: IPos): Boolean {
+    return pos.y in indices && pos.x in get(pos.y).indices
+}
+
+fun <T> List<List<T>>.get(pos: IPos) = get(pos.y)[pos.x]
+
+fun <T> List<MutableList<T>>.set(pos: IPos, value: T) {
+    get(pos.y)[pos.x] = value
+}
+
+fun ULong.con(other: ULong) = (toString() + other.toString()).toULong()
+
+fun gcd(a: Long, b: Long): Long {
+    var tmpA = a
+    var tmpB = b
+    while (tmpB > 0) {
+        val tmp = tmpB
+        tmpB = tmpA % tmpB
+        tmpA = tmp
+    }
+    return tmpA
+}
+
+fun lcm(a: Long, b: Long): Long {
+    return a * (b / gcd(a, b))
+}

ca455366a25c979f472b9c0679ed3916466c8711
szabob <szabob@neocore.local>
2025-12-05 10:40:45 +0100

    Optimal try

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,3 +1,6 @@
+import kotlin.math.max
+import kotlin.math.min
+
 fun main() {
     fun part1(input: List<String>): Int {
         var solution = 0
@@ -20,10 +23,41 @@ fun main() {
         return solution
     }
 
-    fun part2(input: List<String>): Int {
-        var solution = 0
+    fun part2(input: List<String>): ULong {
+        var solution: ULong
 
         val freshRanges = mutableListOf<ULongRange>()
+
+        input.takeWhile { it.isNotBlank() }.map {
+            val split = it.split('-')
+            split[0].toULong()..split[1].toULong()
+        }.forEach { range ->
+            run add@{
+                var rangeToAdd = range
+                val rangesToDelete = mutableListOf<ULongRange>()
+                freshRanges.forEach { freshRange ->
+                    if (freshRange.first in rangeToAdd && freshRange.last in rangeToAdd) {
+                        // meglevo benne van
+                        rangesToDelete.add(freshRange)
+                    }
+                    if (rangeToAdd.first in freshRange && rangeToAdd.last in freshRange) {
+                        // benne van egy meglevoben
+                        return@add
+                    }
+                    if (rangeToAdd.first < freshRange.first && rangeToAdd.last > freshRange.first ||
+                        rangeToAdd.first < freshRange.last && rangeToAdd.last > freshRange.last ) {
+                        rangesToDelete.add(freshRange)
+                        rangeToAdd = min(rangeToAdd.first, freshRange.first)..max(rangeToAdd.last, freshRange.last)
+                    }
+                }
+                freshRanges.removeAll(rangesToDelete)
+                freshRanges.add(rangeToAdd)
+            }
+        }
+
+        solution = freshRanges.fold(0UL) { sum, value ->
+            sum + (value.last - value.first) + 1UL
+        }
 
         return solution
     }
@@ -33,10 +67,10 @@ fun main() {
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day05_test")
-    part1(testInput).println()
+    part2(testInput).println()
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day05")
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }

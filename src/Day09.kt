9852162e1ecb554dd7b0c94c3060563961e57fa6
Benxidosz <szabo.bence.sandor@gmail.com>
2025-12-11 09:18:44 +0100

    Day09 solution 2

--- src/Day09.kt
+++ src/Day09.kt
@@ -43,55 +43,64 @@ fun main() {
         }
 
         tiles.add(tiles.first())
-        val cyclicTiles = tiles.zipWithNext()
-        val horizontalSides = cyclicTiles.filter { side ->
-            side.first.y == side.second.y
-        }.map {
-            IPos(min(it.first.x, it.second.x), it.first.y) to IPos(max(it.first.x, it.second.x), it.second.y)
-        }
-        val verticalSides = cyclicTiles.filter { side ->
-            side.first.x == side.second.x
-        }.map {
-            IPos(it.first.x, min(it.first.y, it.second.y)) to IPos(it.second.x, max(it.first.y, it.second.y))
+        val cyclicTiles = tiles.zipWithNext().map {
+            IPos(min(it.first.x, it.second.x), min(it.first.y, it.second.y)) to
+                    IPos(max(it.first.x, it.second.x), max(it.first.y, it.second.y))
         }
 
         val testValues = mutableMapOf<IPos, Boolean>()
         fun testPoint(point: IPos): Boolean {
             return testValues.getOrPut(point) {
-                horizontalSides.forEach { side ->
-                    if (point in side) {
-                        // Vertical Border
+                cyclicTiles.forEach { side ->
+                    if (side.first.x <= point.x &&
+                        side.first.y <= point.y &&
+                        side.second.x >= point.x &&
+                        side.second.y >= point.y
+                    ) {
                         return@getOrPut true
                     }
                 }
-                var intersection = 0
-                verticalSides.forEach { side ->
-                    if (point in side) {
-                        // Vertical Border
-                        return@getOrPut true
-                    }
 
-                    if (point.x > side.first.x && point.y > side.first.y && point.y <= side.second.y) {
-                        ++intersection
-                    }
+                var crossings = 0
+                cyclicTiles.forEach { side ->
+                    if (side.first.y == side.second.y) return@forEach
+
+                    if (point.x < side.first.x &&
+                        side.first.y < point.y &&
+                        side.second.y >= point.y
+                    ) ++crossings
                 }
-                return@getOrPut intersection % 2 == 1
+
+                return@getOrPut crossings % 2 == 1
             }
         }
 
         fun testRect(rect: Pair<IPos, IPos>): Boolean {
             val minCorner = IPos(min(rect.first.x, rect.second.x), min(rect.first.y, rect.second.y))
-            val a = IPos(min(rect.first.x, rect.second.x), max(rect.first.y, rect.second.y))
-            val b = IPos(max(rect.first.x, rect.second.x), min(rect.first.y, rect.second.y))
             val maxCorner = IPos(max(rect.first.x, rect.second.x), max(rect.first.y, rect.second.y))
 
-            if (!(testPoint(minCorner) && testPoint(a) && testPoint(b) && testPoint(maxCorner))) return false
+            val corners = listOf(minCorner, IPos(minCorner.x, maxCorner.y), IPos(maxCorner.x, minCorner.y), maxCorner)
+            if (!corners.all { testPoint(it) }) {
+                return false
+            }
 
-            for (x in minCorner.x + 1..< maxCorner.x) {
-                for (y in minCorner.y + 1..< maxCorner.y) {
-                    if (!testPoint(IPos(x, y))) {
-                        return false
-                    }
+            cyclicTiles.forEach { side ->
+                if (side.first.x < maxCorner.x && minCorner.x < side.second.x &&
+                    side.first.y < maxCorner.y && minCorner.y < side.second.y
+                ) {
+                    return false
+                }
+            }
+
+            if (maxCorner.x - minCorner.x > 1 && maxCorner.y - minCorner.y > 1) {
+                val innerCorners = listOf(
+                    IPos(minCorner.x + 1, minCorner.y + 1),
+                    IPos(minCorner.x + 1, maxCorner.y - 1),
+                    IPos(maxCorner.x - 1, minCorner.y + 1),
+                    IPos(maxCorner.x - 1, maxCorner.y - 1)
+                )
+                if (!innerCorners.all { testPoint(it) }) {
+                    return false
                 }
             }
 
@@ -107,16 +116,16 @@ fun main() {
         }
     }
 
-    // Test if implementation meets criteria from the description, like:
+        // Test if implementation meets criteria from the description, like:
 //    check(part1(listOf("test_input")) == 3)
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day09_test")
-    part1(testInput).println()
-    part2(testInput).println()
+        // Or read a large test input from the `src/Day01_test.txt` file:
+        val testInput = readInput("Day09_test")
+        part1(testInput).println()
+        part2(testInput).println()
 
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day09")
-    part1(input).println()
-    part2(input).println()
-}
+        // Read the input from the `src/Day01.txt` file.
+        val input = readInput("Day09")
+        part1(input).println()
+        part2(input).println()
+    }
